1008a1c8f877ed92b9a243f7f529c6b55748c6ee
AgentFloro <floro.av@obmontessori.edu.ph>
2026-09-17 18:01:14 +0800

    MASTARD REMAKE

--- main.py
+++ main.py
@@ -3,5 +3,17 @@ from pyscript import document, display
 def place_order(e):
        document.getElementById("output1").innerHTML ="" 
        Main = document.getElementById("Main")
+       Appetize = document.getElementById("Appetize")
+       Drink = document.getElementById("Drink")
        Main_price = float(Main.value)
-       display(Main_price, target="output1") 
+       Appe_price = float(Appetize.value)
+       Drink_price = float(Drink.value)
+       total = Main_price + Appe_price + Drink_price
+       VAT = total * 0.12
+       SuperTotal = total + VAT
+       display(f"[[Thank you For your Order!]]", target="output1") 
+       display(f"[[Receipt]]", target="output1") 
+       display(f"Subtotal: {total}", target="output1") 
+       display(f"VAT: {VAT}", target="output1") 
+       display(f"Total: {SuperTotal}", target="output1") 
+       display(f"Please come back again!", target="output1") 
